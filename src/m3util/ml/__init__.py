1b829b0d6755d8ab8a96909df5a370fdd46765d9
jagar2 <jca92@drexel.edu>
2024-09-06 14:10:28 -0400

    #patch add optimizers

--- src/m3util/ml/__init__.py
+++ src/m3util/ml/__init__.py
@@ -4,5 +4,6 @@ from . import logging
 from . import inference
 from . import rand
 from . import preprocessor
+from . import optimizers
 
-__all__ = ['logging', 'inference', 'rand', 'preprocessor']
+__all__ = ['logging', 'inference', 'rand', 'preprocessor', 'optimizers']
--- src/m3util/ml/optimizers/__init__.py
+++ src/m3util/ml/optimizers/__init__.py
@@ -1,7 +1,6 @@
 # Auto-generated __init__.py
 
 from . import TrustRegion
-from . import TRPCGOptimizerv2
 from . import AdaHessian
 
-__all__ = ['TrustRegion', 'TRPCGOptimizerv2', 'AdaHessian']
+__all__ = ['TrustRegion', 'AdaHessian']
