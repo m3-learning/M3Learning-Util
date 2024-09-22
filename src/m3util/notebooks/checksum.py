d8073e900cae85a117af5269324ea9d22e324259
jagar2 <jca92@drexel.edu>
2024-09-22 14:47:45 +0000

    remove select hash algorithm

--- src/m3util/notebooks/checksum.py
+++ src/m3util/notebooks/checksum.py
@@ -24,6 +24,8 @@ def calculate_notebook_code_checksum(notebook_path, algorithm="sha256"):
         print(checksum)
     """
 
+    hash_function = select_hash_algorithm(algorithm)
+    
     # Load the notebook as a JSON object
     with open(notebook_path, "r", encoding="utf-8") as f:
         notebook = json.load(f)
@@ -38,6 +40,44 @@ def calculate_notebook_code_checksum(notebook_path, algorithm="sha256"):
             # Join the source lines of the code cell (if it's not empty)
             code_cells_content += "".join(cell.get("source", [])) + "\n"
 
+
+    # Encode the concatenated code cells content as bytes and update the hash function
+    hash_function.update(code_cells_content.encode("utf-8"))
+
+    # Return the checksum as a hexadecimal string
+    return hash_function.hexdigest()
+
+
+
+def calculate_h5file_checksum(file_path, algorithm="sha256"):
+    """
+    Calculate the checksum of an HDF5 (.h5) file using the specified hashing algorithm.
+
+    This function reads the binary content of the HDF5 file and computes its checksum using
+    one of the supported hashing algorithms: 'sha256', 'md5', or 'sha1'.
+
+    Args:
+        file_path (str): The path to the HDF5 file (.h5) to be hashed.
+        algorithm (str): The hashing algorithm to use. Supported options are 'sha256' (default), 'md5', and 'sha1'.
+
+    Returns:
+        str: The computed checksum as a hexadecimal string.
+
+    Raises:
+        ValueError: If an unsupported hashing algorithm is specified.
+    """
+
+    hash_function = select_hash_algorithm(algorithm)
+
+    # Open the file in binary mode and compute its checksum
+    with open(file_path, "rb") as f:
+        while chunk := f.read(8192):  # Read the file in chunks (8192 bytes)
+            hash_function.update(chunk)
+
+    # Return the checksum as a hexadecimal string
+    return hash_function.hexdigest()
+
+def select_hash_algorithm(algorithm):
     # Select the hashing algorithm based on the user input
     if algorithm == "sha256":
         hash_function = hashlib.sha256()
@@ -50,9 +90,4 @@ def calculate_notebook_code_checksum(notebook_path, algorithm="sha256"):
         raise ValueError(
             "Unsupported hashing algorithm. Use 'sha256', 'md5', or 'sha1'."
         )
-
-    # Encode the concatenated code cells content as bytes and update the hash function
-    hash_function.update(code_cells_content.encode("utf-8"))
-
-    # Return the checksum as a hexadecimal string
-    return hash_function.hexdigest()
+    return hash_function
