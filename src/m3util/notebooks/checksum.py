d122a132c98574fd38d3059c27f1af2d6da39e3b
jagar2 <jca92@drexel.edu>
2024-09-23 01:37:08 +0000

    working on datafed reference

--- src/m3util/notebooks/checksum.py
+++ src/m3util/notebooks/checksum.py
@@ -1,8 +1,7 @@
 import json
-import hashlib
+from m3util.util.hashing import select_hash_algorithm
 
-
-def calculate_notebook_code_checksum(notebook_path, algorithm="sha256"):
+def calculate_notebook_checksum(notebook_path, algorithm="sha256"):
     """
     Calculate the checksum of the code cells in a Jupyter notebook (.ipynb) file using the specified hashing algorithm.
 
@@ -20,7 +19,7 @@ def calculate_notebook_code_checksum(notebook_path, algorithm="sha256"):
         ValueError: If an unsupported hashing algorithm is specified.
 
     Example:
-        checksum = calculate_notebook_code_checksum("/path/to/notebook.ipynb", algorithm="sha256")
+        checksum = calculate_notebook_checksum("/path/to/notebook.ipynb", algorithm="sha256")
         print(checksum)
     """
 
@@ -47,47 +46,3 @@ def calculate_notebook_code_checksum(notebook_path, algorithm="sha256"):
     # Return the checksum as a hexadecimal string
     return hash_function.hexdigest()
 
-
-
-def calculate_h5file_checksum(file_path, algorithm="sha256"):
-    """
-    Calculate the checksum of an HDF5 (.h5) file using the specified hashing algorithm.
-
-    This function reads the binary content of the HDF5 file and computes its checksum using
-    one of the supported hashing algorithms: 'sha256', 'md5', or 'sha1'.
-
-    Args:
-        file_path (str): The path to the HDF5 file (.h5) to be hashed.
-        algorithm (str): The hashing algorithm to use. Supported options are 'sha256' (default), 'md5', and 'sha1'.
-
-    Returns:
-        str: The computed checksum as a hexadecimal string.
-
-    Raises:
-        ValueError: If an unsupported hashing algorithm is specified.
-    """
-
-    hash_function = select_hash_algorithm(algorithm)
-
-    # Open the file in binary mode and compute its checksum
-    with open(file_path, "rb") as f:
-        while chunk := f.read(8192):  # Read the file in chunks (8192 bytes)
-            hash_function.update(chunk)
-
-    # Return the checksum as a hexadecimal string
-    return hash_function.hexdigest()
-
-def select_hash_algorithm(algorithm):
-    # Select the hashing algorithm based on the user input
-    if algorithm == "sha256":
-        hash_function = hashlib.sha256()
-    elif algorithm == "md5":
-        hash_function = hashlib.md5()
-    elif algorithm == "sha1":
-        hash_function = hashlib.sha1()
-    else:
-        # Raise an error if the algorithm is not supported
-        raise ValueError(
-            "Unsupported hashing algorithm. Use 'sha256', 'md5', or 'sha1'."
-        )
-    return hash_function
