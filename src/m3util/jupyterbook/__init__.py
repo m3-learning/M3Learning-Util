cb57b0ef25dda1af16d267b272d3f312afe21209
jagar2 <jca92@drexel.edu>
2024-12-14 19:36:31 -0500

    add remove bold headings

--- src/m3util/jupyterbook/__init__.py
+++ src/m3util/jupyterbook/__init__.py
@@ -1,6 +1,6 @@
 # Auto-generated __init__.py
 
-from . import make_index, organize_folder
+from . import make_index, organize_folder, remove_bold_headings
 
 
-__all__ = ["make_index", "organize_folder"]
+__all__ = ["make_index", "organize_folder", "remove_bold_headings"]
